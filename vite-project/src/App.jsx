56fdbe1f829522ffd1c3285938356f36eaf50f84
Geetha sri kotari <23A91A0529@aec.edu.in>
2025-09-02 11:03:55 +0530

    temp commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './Home.jsx'
+
+function App() {
+  
+  return (
+    <>
+      <div className="app-whole">
+        <Home />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.css'
+import Header from './Header.jsx'
+
+const Home = () => {
+    return (
+        <>
+        <div className="home-whole">
+            <Header />
+        </div>
+        </>
+    )
+}
+
+export default Home;
