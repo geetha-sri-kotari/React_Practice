e814661b41ac9734932fa2c5f8249c4f0461cc55
Geetha sri kotari <23A91A0529@aec.edu.in>
2025-09-19 11:50:37 +0530

    use state commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,40 +7,43 @@ import Cards_practice from './Cards_practice.jsx'
 import UseState_practice from './UseState_practice.jsx'
 
 function App() {
-  // var MyCards = [
-  //   {
-  //     "Name" : 'Geetha',
-  //     "Occupation" : 'ML Engineer',
-  //     "Salary" : '1CPA'
-  //   },
-  //   {
-  //     "Name" : 'Sri',
-  //     "Occupation" : 'Full Stack Developer',
-  //     "Salary" : '50LPA'
-  //   },
-  //   {
-  //     "Name" : 'Suneetha',
-  //     "Occupation" : 'Marketing Manager',
-  //     "Salary" : '1CPA'
-  //   },
-  //   {
-  //     "Name" : 'Sai',
-  //     "Occupation" : 'Spiritual Guider',
-  //     "Salary" : '10000...'
-  //   }
+  var MyCards = [
+    {
+      "Name" : 'Geetha',
+      "Occupation" : 'ML Engineer',
+      "Salary" : '1CPA'
+    },
+    {
+      "Name" : 'Sri',
+      "Occupation" : 'Full Stack Developer',
+      "Salary" : '50LPA'
+    },
+    {
+      "Name" : 'Suneetha',
+      "Occupation" : 'Marketing Manager',
+      "Salary" : '1CPA'
+    },
+    {
+      "Name" : 'Sai',
+      "Occupation" : 'Spiritual Guider',
+      "Salary" : '10000...'
+    }
 
-  // ];
+  ];
   return (
     <>
-      {/*<Form />*/}
-      {/* <div className='Cards_Parent'>
+      <Form />
+      <br/>
+       <div className='Cards_Parent'>
       {
         MyCards.map((ele) => {
           return <Cards_practice  Data = {ele}/>
         })
       }
-      </div> */}
+      </div> 
+      <br/>
       <UseState_practice />
+      <br/>
     </>
   )
 }
