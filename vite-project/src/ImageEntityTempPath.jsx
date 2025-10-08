110f68ac894b28f7a7092d732ffd5b1498584517
Geetha sri kotari <23A91A0529@aec.edu.in>
2025-10-08 11:17:09 +0530

    Image Entities using Reader File  (base 64)

--- vite-project/src/ImageEntityTempPath.jsx
+++ vite-project/src/ImageEntityTempPath.jsx
@@ -2,17 +2,36 @@ import React from "react";
 import { useState } from "react";
 const ImageEntityTempPath = () => {
     const [ImagePath, setImagePath] = useState(null);
+    const [ReaderImagePath, setReaderImagePath] = useState(null)
     const GetTempPath = (event) => {
-        const file = event.target.files[0];
-        const path = URL.createObjectURL(file)
+        const file1 = event.target.files[0];
+        const path = URL.createObjectURL(file1)
         setImagePath(path)
+        
+    }
+    const GetReaderPath = (event) => {
+        const file2 = event.target.files[0];
+        const reader = new FileReader()
+        reader.readAsDataURL(file2)
+        reader.onload = () => {
+            setReaderImagePath(reader.result)
+            console.log(reader.result)
+        }
     }
     return (
         <>
+        <h3>Using Temporary Path</h3>
         <input type="file"  onChange={(event) => GetTempPath(event)}/>
         <br/>
         {
             ImagePath ? <img src={ImagePath} className="image-entity" alt="Image Cracked"/> : <></>
+        }
+        <br />
+        <h3>Using Reader </h3>
+        <input type="file"  onChange={(event) => GetReaderPath(event)}/>
+        <br/>
+        {
+            ReaderImagePath ? <img src={ReaderImagePath} className="image-entity" alt="Image Cracked"/> : <></>
         }
         </>
     )
